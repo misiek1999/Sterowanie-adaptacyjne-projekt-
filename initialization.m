8c14c11c6a3a22b683f63d61d65c053fef85fd8b
misiek1999 <michaldydo99@gmail.com>
2022-11-17 22:46:43 +0100

    Add new simulation model and observer Calculate offline G1 and G2

--- initialization.m
+++ initialization.m
@@ -1,13 +1,20 @@
 %% load system from other directory
 path_to_model = 'model/load_model.m';
 run(path_to_model);
+
+x0 = [0 0];
+
 % determine the row of the matrix
 n = length(A);
 global A B C n W M 
+global T time_step time_stamps time_samples_count
+global G1_vec G2_vec
 %% Define integral observer parameters
 betas = [1 1];
 T = 5;
-
+time_step = 0.01;
+time_stamps = 0 : time_step :T;
+time_samples_count = length(time_stamps);
 
 %% find best G1 and G2 matrix
 % initialize P matrix's
@@ -27,12 +34,14 @@ for i = 1:n
     M{i} = Mi;
 end
 
-
-% % transpose Pi1 and Pi2
-% Pi1 = Pi1.';
-% Pi2 = Pi2.';
-% 
-% %% calculate G matrix's
-% G1 = P1 * C.';
-% G2 = P2 * B;
-
+%%  Create vector with G1 and G2
+G1_vec = [];
+G2_vec = [];
+[g1_n g1_m] = size(getG1(0));
+[g2_n g2_m] = size(getG2(0));
+itr = 1;
+for t = time_stamps
+    G1_vec = [G1_vec getG1(t)];
+    G2_vec = [G2_vec getG2(t)]; 
+    itr = itr + 1;
+end
