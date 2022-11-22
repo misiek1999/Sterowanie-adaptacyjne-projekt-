3ebd341a333517315f06c8586fa00b8beb001849
kmariuszair <mariusz.kuchta@onet.com.pl>
2022-11-22 16:33:27 +0100

    Make cost function graph depending on betas and window length

--- symbolic/symbolic_calculations.m
+++ symbolic/symbolic_calculations.m
@@ -5,15 +5,25 @@ run("model_params.m");
 
 sizeA = size(A);
 system_size = sizeA(2);  % liczba zmiennych stanu
+sizeC = size(C);
+output_size = sizeC(1);  % liczba wyjsc
+sizeB = size(B);
+input_size = sizeB(2);  % liczba wejsc
 
 syms t tau T real positive
+% assume(t>=0);
+% assume(tau>=0);
+% assume(T>0);
 
 %%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% wzór ogólny (około 10 minut liczenia dla najogólniejszego przypadku)
-syms beta [1 system_size] real positive
-beta(1) = 1;
-beta(2) = 1;
+syms beta [1 system_size] real
+for i=1:system_size
+    assume(beta(i)>0);
+end
+% beta(1) = 0;
+% beta(2) = 0;
 syms J
 P1 = [];
 P2 = [];
@@ -38,65 +48,81 @@ for i = 1:system_size
     P2 = [P2, p2]
 end
 
-G1 = P1' * C.';  % transponuję zmienną P1, ale w teorii jest to macierz P1 nietransponowana
-G2 = P2' * B;
+G1 = P1' * C.'  % transponuję zmienną P1, ale w teorii jest to macierz P1 nietransponowana
+G2 = P2' * B
 
-G1 = simplify(G1,'steps',1000);
-G2 = simplify(G2,'steps',1000);
+G1 = simplify(G1,'steps',1000)
+G2 = simplify(G2,'steps',1000)
 
 % przykladowe przebiegi składowych macierzy G1 i G2
 %%
 Treal = 0.5;
+betasreal = [0.5 0.5];
+
 figure(1)
+for i = 1:system_size
+    for j = 1:output_size
+        G1tempf = subs(G1(i,j), T, Treal);
+        for k = 1:system_size
+            G1tempf = subs(G1tempf, beta(k), betasreal(k));
+        end
+        G1tempf = matlabFunction(G1tempf);
+        subplot(system_size,output_size,j+(i-1)*output_size)
+        plot(0:0.01:Treal,G1tempf(0:0.01:Treal))
+        grid on;
+        title(["G1 "+num2str(i)+num2str(j)])
+    end
+end
+
 figure(2)
 for i = 1:system_size
-    G1tempf = matlabFunction(subs(G1(i), T, Treal));
-    G2tempf = matlabFunction(subs(G2(i), T, Treal));
-    figure(1)
-    subplot(system_size,1,i)
-    plot(0:0.01:Treal,G1tempf(0:0.01:Treal))
-    grid on;
-    title(["G1"+num2str(i)])
-    figure(2)
-    subplot(system_size,1,i)
-    plot(0:0.01:Treal,G2tempf(0:0.01:Treal))
-    grid on;
-    title(["G2"+num2str(i)])
+    for j = 1:input_size
+        G2tempf = subs(G2(i,j), T, Treal);
+        for k = 1:system_size
+            G2tempf = subs(G2tempf, beta(k), betasreal(k));
+        end
+        G2tempf = matlabFunction(G2tempf);
+        subplot(system_size,output_size,j+(i-1)*output_size)
+        plot(0:0.01:Treal,G2tempf(0:0.01:Treal))
+        grid on;
+        title(["G2 "+num2str(i)+num2str(j)])
+    end
 end
 
 %%
 % dodatkowy FOR do liczenia wskaźnika jakości, bardzo wydłuża czas obliczeń
+% syms betashat [1 system_size] real positive
 for i = 1:system_size
-    J = sum(G1(i,:).^2) + beta(i)*sum(G2(i,:).^2)
+    J = sum(G1(i,:).^2) + 1*sum(G2(i,:).^2)  % koszt liczymy jakby na wejściu były zakłócenia
 end
 J = simplify(J,'steps',1000)
 J = int(J, t, 0, T)
-J = simplify(J,'steps',1000)
-Jf = matlabFunction(J)
+% J = simplify(J,'steps',1000)
 
+%%
+Jfs    = [];
+Trange = 0:0.1:10;
+
+for betasfortest = 0.01:0.01:1.01
+    Jf = J;
+    for k = 1:system_size  % liczba parametrów beta
+        Jf = subs(J, beta(k), betasfortest);
+    end
+    Jf = matlabFunction(Jf);
+    Jfs = [Jfs; Jf(Trange)];
+end
+%%
+sizeJfs = size(Jfs);
 figure(4)
-plot(0:0.01:10, Jf(0:0.01:10))
+X = meshgrid(Trange);
+X = X(1:101,:);
+Y = meshgrid(0.0:0.01:1.0)';
+Y = Y(1:101,:);
+mesh(X, Y, Jfs)
 grid on;
-title("Funkcja kosztu w zależności od T dla \beta_1="+num2str(double(beta(1)))+...
-    " \beta_2="+num2str(double(beta(2))))
 xlabel("T")
-ylabel("Wartość funkcji kosztu")
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  przykład strona 318
-% beta = [1 1];
-% W = [A,    B*B'
-%     C'*C, -A']
-% Phi = expm(W*t)
-% Phi = simplify(Phi,'steps',1000)
-% Phi11 = Phi(1:system_size,1:system_size)
-% Phi21 = Phi(system_size+1:2*system_size,1:system_size)
-% M = int(subs(Phi11.',t,tau)*C.'*C*expm(A*tau),tau,0,T) * expm(-A*T)
-% M = simplify(M,'steps',1000)
-% iM = inv(M)
-% G1 = iM * Phi11.' * C.'
-% G2 = iM * Phi21.' * B
-% 
-% J = G1(1,1)^2 + G1(2,1)^2;
-% J = J + G2(1,1)^2 + G2(2,1)^2;
-% J = int(J, t, 0, T)
-% Jsimp = simplify(J,'steps',1000)
+ylabel("$\hat\beta(1)=\hat\beta(2)=...=Y$", 'Interpreter', 'latex')
+zlabel("Wartość funkcji kosztu")
+zlim([0, 5])
+caxis([0 3])
+colorbar
