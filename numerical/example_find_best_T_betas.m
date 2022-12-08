b2cc7e11f4c85e6f3eefe59db450c0db9e810cd9
kmariuszair <mariusz.kuchta@onet.com.pl>
2022-12-08 23:51:55 +0100

    Poprawienie komentarza

--- numerical/example_find_best_T_betas.m
+++ numerical/example_find_best_T_betas.m
@@ -2,7 +2,7 @@ example_params;
 
 options = optimset('MaxIter',500,'PlotFcns',@optimplotfval);
 
-params0 = [1, 0.5, 0.5];  % T0, beta1, beta2 - długość zależy od wymiaru macierzy A
+params0 = [1, 0.2, 0.2];  % T0, beta1, beta2, ..., beta_n_B - długość zależy od wymiaru macierzy B
 optifhand = @(p)optiG1G2(A, B, C, p);
 
 [x,fval,exitflag,output] = fminsearch(optifhand, params0, options);
