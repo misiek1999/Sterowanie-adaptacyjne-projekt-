0261e07a6f8a890f03150c63dccbfda25266aabe
misiek1999 <michaldydo99@gmail.com>
2022-12-12 17:37:30 +0100

    Litte speed up basic fucntion to find G1 and G2

--- getG1.m
+++ getG1.m
@@ -2,14 +2,14 @@ function [G1] = getG1(t)
     % function to calcluate new G1 matrix
     global A B C n W M 
     
-    P1 = [];
+    P1 = zeros(n);
     for i = 1:n
         ei = zeros([n 1]);
         ei(i) = 1;
 
         p = getPhi11(t, i) /M{i}*ei;
         
-        P1 = [P1 p];
+        P1(:,i) = p;
     end
 
     G1 = P1.'*C.';
